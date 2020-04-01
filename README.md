2422492310ca4fece1e549626785ee8c0c765c72
raza8899 <amirraza2221@gmail.com>
2020-04-01 17:20:30 +0200

    Build App

--- README.md
+++ README.md
@@ -2,8 +2,10 @@ This project was bootstrapped with [Create React App](https://github.com/faceboo
 
 ## Available Scripts
 
-In the project directory, you can run:
+In the project directory, you can run: 
 
+### `npm install` to get all required dependencies
+ Then 
 ### `yarn start`
 
 Runs the app in the development mode.<br />
---
+++ build/precache-manifest.db5875593a1a7003a71257f2c056a6c5.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddc4e4ac08d7f47ed114e7a6c6e36b4b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "78e60715b578e0e6d7de",
+    "url": "/static/css/2.dae0f90d.chunk.css"
+  },
+  {
+    "revision": "37aa6ad8574689c88a3b",
+    "url": "/static/css/main.a5763f1e.chunk.css"
+  },
+  {
+    "revision": "78e60715b578e0e6d7de",
+    "url": "/static/js/2.cae45e03.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.cae45e03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37aa6ad8574689c88a3b",
+    "url": "/static/js/main.deab0ab0.chunk.js"
+  },
+  {
+    "revision": "6f619bf8eb9421f4ccc2",
+    "url": "/static/js/runtime-main.3afe4df8.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
